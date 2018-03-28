72afbe2ac1ea5f014ead3512008838525b659413
None <m3tac0ma@gmail.com>
2018-03-28 22:34:53 +0400

    fix path to lookup script

--- bin/silverkey.sh
+++ bin/silverkey.sh
@@ -3,4 +3,4 @@
 #key=`curl -s 213.59.223.213:2379/v2/keys/?recursive=true | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -i`
 #key=`cat ~/spaces/thaw/data/* | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -i`
 key=`curl -s ${SILVERKEY_HOST}/v2/keys/?recursive=true | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -mesg Keys -markup -i`
-silvercomplete-lookup.sh $key nodel
+silverkey-lookup.sh $key nodel
