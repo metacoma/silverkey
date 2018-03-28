6bc85d1fc7a23122cc2f9ad5e4be858fe90e0a58
None <m3tac0ma@gmail.com>
2018-03-28 23:51:38 +0400

    switch to etcdsimple client

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,7 @@ RUN apk add --update git
 WORKDIR /go/src/app
 COPY src .
 
-RUN go get github.com/coreos/etcd/client
-RUN go get -d -v ./...
+RUN go get github.com/octoblu/go-simple-etcd-client/etcdclient
 RUN go install -v ./...
 RUN go build
 #CMD ["app"]
--- src/silverkey-client.go
+++ src/silverkey-client.go
@@ -2,45 +2,29 @@ package main
 
 import (
 	"log"
-	"time"
-	"context"
   "os"
 
-	"github.com/coreos/etcd/client"
+	"github.com/octoblu/go-simple-etcd-client/etcdclient"
 )
 
 var ETCD_SERVER = os.Getenv("SILVERKEY_HOST")
 
 func main() {
-	cfg := client.Config{
-		Endpoints:               []string{ETCD_SERVER},
-		Transport:               client.DefaultTransport,
-		// set timeout per request to fail fast when the target endpoint is unavailable
-		HeaderTimeoutPerRequest: time.Second,
-	}
-	c, err := client.New(cfg)
+
+  client, err := etcdclient.Dial(ETCD_SERVER)
+
 	if err != nil {
 		log.Fatal(err)
-	}
-	kapi := client.NewKeysAPI(c)
-	// set "/foo" key with "bar" value
-	log.Print("Setting '/foo' key with 'bar' value")
-	resp, err := kapi.Set(context.Background(), "/foo", "bar", nil)
+  }
+
+  nodes, err := client.LsRecursive("/")
+
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		// print common key info
-		log.Printf("Set is done. Metadata is %q\n", resp)
-	}
-	// get "/foo" key's value
-	log.Print("Getting '/foo' key value")
-	resp, err = kapi.Get(context.Background(), "/foo", nil)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		// print common key info
-		log.Printf("Get is done. Metadata is %q\n", resp)
-		// print value
-		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
-	}
+  }
+
+  for _, node := range nodes {
+		log.Printf("Key %s\n", node)
+  }
+
 }
