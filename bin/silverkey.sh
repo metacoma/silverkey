d13fb8841b621850e0b6e3f4356a02f7aa708d33
None <m3tac0ma@gmail.com>
2018-03-28 22:34:33 +0400

    use SILVERKEY_HOST instead SILVERCOMPLETE_HOST

--- bin/silverkey.sh
+++ bin/silverkey.sh
@@ -2,5 +2,5 @@
 
 #key=`curl -s 213.59.223.213:2379/v2/keys/?recursive=true | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -i`
 #key=`cat ~/spaces/thaw/data/* | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -i`
-key=`curl -s ${SILVERCOMPLETE_HOST}/v2/keys/?recursive=true | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -mesg Keys -markup -i`
+key=`curl -s ${SILVERKEY_HOST}/v2/keys/?recursive=true | jq -r '..|.key?' | grep -v '^null$' | rofi -dmenu -mesg Keys -markup -i`
 silvercomplete-lookup.sh $key nodel
