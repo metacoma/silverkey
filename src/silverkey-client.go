16a4cd535fa95ca356468821151104a0e6a38bb2
None <m3tac0ma@gmail.com>
2018-03-29 21:37:11 +0400

    retab

--- src/silverkey-client.go
+++ src/silverkey-client.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"log"
+  "log"
   "os"
 
-	"github.com/octoblu/go-simple-etcd-client/etcdclient"
-	"github.com/wneo/jlfuzzy"
+  "github.com/octoblu/go-simple-etcd-client/etcdclient"
+  "github.com/wneo/jlfuzzy"
 
 )
 
 var ETCD_SERVER = os.Getenv("SILVERKEY_HOST")
 
 func main() {
-	fuzzy := jlfuzzy.NewJLFuzzy()
+  fuzzy := jlfuzzy.NewJLFuzzy()
 
   client, err := etcdclient.Dial(ETCD_SERVER)
 
-	if err != nil {
-		log.Fatal(err)
+  if err != nil {
+    log.Fatal(err)
   }
 
   nodes, err := client.LsRecursive("/")
-	fuzzy.AddWords(nodes)
+  fuzzy.AddWords(nodes)
 
-	if err != nil {
-		log.Fatal(err)
+  if err != nil {
+    log.Fatal(err)
   }
 
-	/* UI part */
+  /* UI part */
 
-	for _, match := range fuzzy.SearchWord(os.Args[1], 1, -1, 0, 10) {
-		log.Printf("Match %v\n", match)
-	}
+  for _, match := range fuzzy.SearchWord(os.Args[1], 1, -1, 0, 10) {
+    log.Printf("Match %v\n", match)
+  }
 
 
 }
