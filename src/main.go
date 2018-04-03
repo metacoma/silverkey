37ca827dc3416604fc2cffb750e5efe2040f2543
None <m3tac0ma@gmail.com>
2018-04-03 05:55:53 +0400

    webview ui

--- src/main.go
+++ src/main.go
@@ -1,65 +1,25 @@
 package main
 
 import (
-  "log"
+
+  "net/http"
+  "net"
   "os"
+  "log"
+  "fmt"
+
 
   "github.com/octoblu/go-simple-etcd-client/etcdclient"
   "github.com/wneo/jlfuzzy"
+  "github.com/zserge/webview"
+  "github.com/go-vgo/robotgo"
 
-  "github.com/therecipe/qt/widgets"
-  "github.com/therecipe/qt/gui"
-  "github.com/therecipe/qt/core"
 )
 
 var ETCD_SERVER = os.Getenv("SILVERKEY_HOST")
 
-func run_ui() {
-    // Create application
-    app := widgets.NewQApplication(len(os.Args), os.Args)
-
-    // Create main window
-    window := widgets.NewQMainWindow(nil, 0)
-    // Remove title bar from window
-    window.SetWindowFlags(core.Qt__Window|core.Qt__FramelessWindowHint)
-    window.SetMinimumSize2(200, 200)
-    // Register KeyPress event
-    window.ConnectKeyPressEvent(func(event *gui.QKeyEvent) {
-        key := event.Key()
-        // If user pressed Return key close UI part
-        if key == 16777220 {
-            app.QuitDefault()
-        } else {
-            log.Printf("Entered Key: %v\n", key)
-        }
-    })
-
-    // Create main layout
-    layout := widgets.NewQVBoxLayout()
-
-    // Create main widget and set the layout
-    mainWidget := widgets.NewQWidget(nil, 0)
-    mainWidget.SetLayout(layout)
-
-    // Create a line edit and add it to the layout
-    input := widgets.NewQLineEdit(nil)
-    input.SetPlaceholderText("1. write something")
-    layout.AddWidget(input, 0, 0)
-
-    // Create list view for search result
-    resultList := widgets.NewQListView(nil)
-    data := []string{"as","asd","asd"}
-    resultList.SetModel(core.NewQStringListModel2(data, nil))
-    layout.AddWidget(resultList, 0, 0)
-
-    // Set main widget as the central widget of the window
-    window.SetCentralWidget(mainWidget)
-
-    // Show the window
-    window.Show()
-
-    // Execute app
-    app.Exec()
+func handler(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "<html><body><input type=text name=key></input><br><select name=nodes><option value=foobar>foobar</option></select></body></html>")
 }
 
 func main() {
@@ -77,12 +37,28 @@ func main() {
   if err != nil {
     log.Fatal(err)
   }
+  robotgo.TypeString("Hello World")
+
+      ln, err := net.Listen("tcp", "127.0.0.1:0")
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer ln.Close()
+        go func() {
+                http.HandleFunc("/", handler)
+                http.Serve(ln, nil)
+        }()
+
+        webview.Open("Silverkey", "http://"+ln.Addr().String()+"/", 400, 300, false)
+
 
   /* UI part */
 
+  /*
   for _, match := range fuzzy.SearchWord(os.Args[1], 1, -1, 0, 10) {
     log.Printf("Match %v\n", match)
   }
+  */
 
 
 }
