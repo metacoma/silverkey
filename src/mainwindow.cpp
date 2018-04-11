cc4ee325fc5a6b58fbb14adcb1c977d654beaf14
None <m3tac0ma@gmail.com>
2018-04-12 03:45:10 +0400

    comlpeter + etcd recursive

---
+++ src/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "etcd.hpp"
+#include "rapid_reply.hpp"
+#include <QLineEdit>
+#include <QCompleter>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    QStringList wordList;
+
+    //etcd::Client<example::RapidReply> etcd = new etcd::Client("localhost", 2379);
+    etcd::Client<example::RapidReply> etcd_client("localhost", 2379);
+    example::RapidReply reply = etcd_client.GetAll("/");
+
+    //reply.KvPairs
+    std::map<std::string, std::string> kvpairs;
+
+    //example::RapidReply::GetAll(kvpairs);
+    reply.GetAll(kvpairs);
+
+    for (auto iter = kvpairs.begin(); iter != kvpairs.end(); ++iter) {
+        //wordList << "hello";
+        wordList << QString::fromStdString(iter->first);
+    }
+
+
+    QLineEdit *lineEdit = new QLineEdit(this);
+
+    QCompleter *completer = new QCompleter(wordList, this);
+    completer->setCaseSensitivity(Qt::CaseInsensitive);
+    lineEdit->setCompleter(completer);
+
+
+
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
