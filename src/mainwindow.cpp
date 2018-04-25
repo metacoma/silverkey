c94834f611a5d445f215483bf5c06ab44c9f9ea1
Dmitry Ukov <dukov@mirantis.com>
2018-04-25 15:24:43 +0400

    Added completer with custom search engine
    This will allow to implement Fuzzy Search later on
    
    Change-Id: I5a27309cdbaa03ac34efe0f9089bb76436fc148a

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -9,11 +9,57 @@
 #include <QDebug>
 #include <QAbstractItemView>
 
+
+void FuzzyCompleter::update(QString pattern){
+    QStringList weighted = m_list.filter(pattern, caseSensitivity());;
+    m_model.setStringList(weighted);
+    m_word = pattern;
+    complete();
+
+}
+
+FuzzyLineEdit::FuzzyLineEdit(QWidget *parent)
+    : QLineEdit(parent), c(0)
+{
+}
+
+FuzzyLineEdit::~FuzzyLineEdit()
+{
+}
+
+FuzzyCompleter *FuzzyLineEdit::completer() const {
+    return c;
+}
+
+void FuzzyLineEdit::setCompleter(FuzzyCompleter *completer) {
+    if (c) {
+        disconnect(c, 0, this, 0);
+        c->setWidget(0);
+        if (c->parent() == this) {
+            delete c;
+        }
+    }
+    c = completer;
+
+    if (!c) {
+        return;
+    }
+
+    if (c->widget() == 0) {
+        c->setWidget(this);
+    }
+
+    QObject::connect(this->completer(), SIGNAL(activated(QString)),
+                     this, SLOT(setText(QString)));
+    QObject::connect(this->completer(), SIGNAL(highlighted(QString)),
+                     this, SLOT(setText(QString)));
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
 
     ui(new Ui::MainWindow),
-    lineEdit(new QLineEdit(this))
+    lineEdit(new FuzzyLineEdit(this))
 {
     setStyleSheet("background:transparent;");
     setAttribute(Qt::WA_TranslucentBackground);
@@ -41,7 +87,7 @@ MainWindow::MainWindow(QWidget *parent) :
     lineEdit->setStyleSheet("QLineEdit {background-color: white;}");
 
 
-    QCompleter *completer = new QCompleter(wordList, this);
+    FuzzyCompleter *completer = new FuzzyCompleter(wordList, this);
     completer->setCaseSensitivity(Qt::CaseInsensitive);
     lineEdit->setCompleter(completer);
     QAbstractItemView *abstractItemView = completer->popup();
@@ -49,6 +95,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(lineEdit, &QLineEdit::returnPressed, this, &MainWindow::EnterPressed);
     connect(abstractItemView, &QAbstractItemView::clicked, this, &MainWindow::EnterPressed);
+
+    connect(lineEdit, &QLineEdit::textEdited, this, &MainWindow::SearchEvent);
 
     this->activateWindow();
     QFocusEvent* eventFocus = new QFocusEvent(QEvent::FocusIn);
@@ -81,10 +129,14 @@ void MainWindow::hideEvent(QHideEvent *e) {
 
 }
 
-
 void MainWindow::EnterPressed() {
     //this->~MainWindow();
     qDebug() << "EnterPressed";
     this->hide();
+}
+
+void MainWindow::SearchEvent() {
+    FuzzyCompleter *c = lineEdit->completer();
+    c->update(lineEdit->text());
 }
 
