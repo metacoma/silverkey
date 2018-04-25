d0c61b0355496064ee6efaddd76a03ff015f764f
Dmitry Ukov <dukov@mirantis.com>
2018-04-25 15:24:43 +0400

    Added completer with custom search engine
    This will allow to implement Fuzzy Search later on
    
    Change-Id: I5a27309cdbaa03ac34efe0f9089bb76436fc148a

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,12 +4,59 @@
 #include <QMainWindow>
 #include <QLineEdit>
 #include <QDebug>
+#include <QCompleter>
+#include <QString>
+#include <QStringList>
+#include <QStringListModel>
 
 #define KEYBOARD_SPEED 20
 
 namespace Ui {
 class MainWindow;
+class FuzzyCompleter;
+class FuzzyLineEdit;
 }
+
+class FuzzyCompleter : public QCompleter
+{
+    Q_OBJECT
+
+public:
+    inline FuzzyCompleter(const QStringList& words, QObject * parent) :
+            QCompleter(parent), m_list(words), m_model()
+    {
+        setModel(&m_model);
+    }
+
+    inline QString word()
+    {
+        return m_word;
+    }
+
+public:
+    void update(QString pattern);
+
+private:
+    QStringList m_list;
+    QStringListModel m_model;
+    QString m_word;
+};
+
+class FuzzyLineEdit : public QLineEdit
+{
+    Q_OBJECT
+
+public:
+    FuzzyLineEdit(QWidget *parent = 0);
+    ~FuzzyLineEdit();
+
+    void setCompleter(FuzzyCompleter *c);
+    FuzzyCompleter *completer() const;
+
+
+private:
+    FuzzyCompleter *c;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -18,17 +65,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     void EnterPressed();
     void EndOfWorkflow();
+    void SearchEvent();
     void hideEvent(QHideEvent *e);
     void setWriteFd(int fd);
 
 private:
     Q_OBJECT
     Ui::MainWindow *ui;
-    QLineEdit *lineEdit;
+    FuzzyLineEdit *lineEdit;
     std::map<std::string, std::string> kvpairs;
     int wfd;
-
-
 };
 
 #endif // MAINWINDOW_H
