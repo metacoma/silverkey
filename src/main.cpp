65c463e7fed2427b8497cec11a5fdbc12ca33af2
Dmitry Ukov <dukov@mirantis.com>
2018-05-24 09:51:10 +0400

    Fork needed only for OSX
    * Window show and keyboard type are isolated to separate functions
    * Introduced new property to LineEdit for keeping result pointer
    
    Change-Id: I8aa372de8d83630214f5cfa2aff2859a9262d61a

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #include "robothelper.h"
 #include <iostream>
 #include <QApplication>
-#include <unistd.h>
-#include <sys/wait.h>
+
+#ifdef Q_OS_MACOS
+# include <unistd.h>
+# include <sys/wait.h>
+#endif
+
 #include <Robot.h>
 #include <QDebug>
 #include <QFuture>
@@ -12,71 +16,93 @@
 ROBOT_NS_USE_ALL;
 
 
+void show_window(int argc, char *argv[], QString *res, int fd = 0, bool child = false) {
+    QApplication a(argc, argv);
+    MainWindow w;
 
-int main(int argc, char *argv[])
-{
+    QFutureWatcher<void> watcher;
+    QObject::connect(&watcher, SIGNAL(finished()), &w, SLOT(handleDataLoad()));
+    QFuture<void> future = QtConcurrent::run(&w, &MainWindow::getDbData);
+    watcher.setFuture(future);
+
+    if (child) {
+        w.setWriteFd(fd);
+    }
+    if (res) {
+        w.setResultPtr(res);
+    }
+    if (argc == 2) {
+        std::string d(argv[1]);
+        w.setData(d);
+    }
+    w.setAttribute(Qt::WA_DeleteOnClose);
+
+    w.show();
+
+    w.raise();  // for MacOS
+    w.activateWindow(); // for Windows
+
+    qDebug() << a.exec();
+}
+
+void type_text(int dbValLen, char dbVal[256]) {
+
+    qDebug() << "end of application workflow";
+
+    if (dbValLen) {
+        qDebug() << dbVal;
+
+        std::string value(dbVal);
+
+        RobotHelper helper;
+        auto converted = helper.convert(value);
+        const char* c_converted = converted.c_str();
+
+        Keyboard keyboard;
+
+        keyboard.AutoDelay.Min = 2;
+        keyboard.AutoDelay.Max = 3;
+
+        keyboard.Click (c_converted);
+        //This one easily misses some key presses. Need to get rid of it asap.
+        //Does not support multiline properly eather.
+    }
+}
+
+void main_with_fork(int argc, char *argv[]) {
     int fd[2];
-    char dbVal[256];
-    int dbValLen;
-    int status;
     pipe(fd);
     pid_t pid = fork();
 
-    memset(&dbVal, 0, sizeof(dbVal));
     if (pid == 0) {
         close(fd[0]);
-        QApplication a(argc, argv);
-        MainWindow w;
-
-        QFutureWatcher<void> watcher;
-        QObject::connect(&watcher, SIGNAL(finished()), &w, SLOT(handleDataLoad()));
-        QFuture<void> future = QtConcurrent::run(&w, &MainWindow::getDbData);
-        watcher.setFuture(future);
-
-        w.setWriteFd(fd[1]);
-        if (argc == 2) {
-            std::string d(argv[1]);
-            w.setData(d);
-        }
-        w.setAttribute(Qt::WA_DeleteOnClose);
-
-        w.show();
-
-        w.raise();  // for MacOS
-        w.activateWindow(); // for Windows
-      
-        qDebug() << a.exec();
+        show_window(argc, argv, NULL, fd[1], true);
         close(fd[1]);
     } else {
         close(fd[1]);
         wait(NULL);
 
         if (argc == 1) {
+            char dbVal[256];
+            int dbValLen;
+            memset(&dbVal, 0, sizeof(dbVal));
             dbValLen = read(fd[0], &dbVal, sizeof(dbVal));
-            qDebug() << "end of application workflow";
-
-            if (dbValLen) {
-                qDebug() << dbVal;
-
-                std::string value(dbVal);
-
-                RobotHelper helper;
-                auto converted = helper.convert(value);
-                const char* c_converted = converted.c_str();
-
-                Keyboard keyboard;
-
-                keyboard.AutoDelay.Min = 2;
-                keyboard.AutoDelay.Max = 3;
-
-                keyboard.Click (c_converted);
-                //This one easily misses some key presses. Need to get rid of it asap.
-                //Does not support multiline properly eather.
-            }
-
+            type_text(dbValLen, dbVal);
         }
         close(fd[0]);
-
     }
+}
+
+
+
+int main(int argc, char *argv[])
+{
+#ifdef Q_OS_MACOS
+    main_with_fork(argc, argv);
+#else
+    QString res = "";
+    show_window(argc, argv, &res);
+    type_text(res.length(), res.toUtf8().constData());
+#endif
     return EXIT_SUCCESS;
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -134,6 +134,11 @@ void MainWindow::getDbData()
     }
 }
 
+void MainWindow::setResultPtr(QString *ptr)
+{
+    resultPtr = ptr;
+}
+
 void MainWindow::lockInput()
 {
     // TODO (dukov) Use gray style here
@@ -173,8 +178,12 @@ void MainWindow::hideEvent(QHideEvent *e) {
             val = kvpairs[key];
         }
         qDebug() << "Hide action, value is " << QString(val.c_str());
-
+#ifdef Q_OS_MACOS
         write(wfd, val.c_str(), std::strlen(val.c_str()));
+#endif
+        if (resultPtr) {
+            *resultPtr = QString::fromStdString(val);
+        }
     }
     e->accept();
     qApp->closeAllWindows();
