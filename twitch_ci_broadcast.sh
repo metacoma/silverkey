b34b19c520a9c07e7d7e18bc403fbeb40bb3cda4
metacoma <m3tac0ma@gmail.com>
2018-06-08 12:50:59 +0400

    update twitch docker container, add loop audio

--- twitch_ci_broadcast.sh
+++ twitch_ci_broadcast.sh
@@ -1,6 +1,8 @@
 streaming() {
-     INRES="1024x648" # input resolution
-     OUTRES="800x600" # output resolution
+     #INRES="1024x648" # input resolution
+     #OUTRES="800x600" # output resolution
+     INRES="1920x1080" # input resolution
+     OUTRES="1920x1080" # output resolution
      FPS="15" # target FPS
      GOP="30" # i-frame interval, should be double of FPS,
      GOPMIN="15" # min i-frame interval, should be equal to fps,
@@ -12,7 +14,9 @@ streaming() {
      _DISPLAY=${DISPLAY:-0}
 
      #ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i ${_DISPLAY}.0 -i ./bicycle.mp3 "amovie=audio.wav:loop=3" -acodec copy -f flv  \
-     ffmpeg  -f x11grab -s "$INRES" -r "$FPS" -i ${_DISPLAY}.0 -stream_loop -1 -i ./bicycle.mp3 -map 0:v:0 -map 1:a:0 -filter_complex loop=-1 -acodec copy -f flv  \
+     #ffmpeg  -f x11grab -s "$INRES" -r "$FPS" -stream_loop -1 -i ${_DISPLAY}.0 -stream_loop -1 -i ./bicycle.mp3 -acodec copy -f flv  \
+     #ffmpeg  -f x11grab -s "$INRES" -r "$FPS" -stream_loop -1 -i ${_DISPLAY}.0 -i ./bicycle.mp3 -filter_complex "[0:a]afifo[a0];[0:a]afifo[a1];[0:a]afifo[a2];[a0][a1][a2]concat=n=3:v=0:a=1[a]" -map "[a]" -acodec copy -f flv  \
+     ffmpeg  -f x11grab -s "$INRES" -r "$FPS" -i ${_DISPLAY}.0 -lavfi "amovie=./rhapsody_in_blue.mp3:loop=4444444,asetpts=N/SR/TB" -strict experimental -f flv  \
        -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p\
        -s $OUTRES -preset $QUALITY -tune film -threads $THREADS -strict normal \
        -bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
