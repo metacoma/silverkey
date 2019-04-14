331aa696fe1957a8bc7ec43a1a0928dadb58899b
Vasiliy Sorokin <vasiliy.sorokin@opensoftdev.ru>
2019-04-14 10:41:02 +0300

    Update build script

--- deploy/build.bat
+++ deploy/build.bat
@@ -3,7 +3,7 @@
 cd /d %~dp0
 
 call "%QTDIR%\bin\qtenv2.bat"
-call "%VS150COMNTOOLS%..\..\VC\Auxiliary\Build\vcvars64.bat"
+call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars64.bat"
 
 cd /d %~dp0
 cd ../src
@@ -11,7 +11,7 @@ cd ../src
 qmake CONFIG+=release
 nmake
 nmake install
-windeployqt --no-translations ../bin/silverkey-qt.exe
+windeployqt --no-translations --force --qmldir ../src/qml ../bin/silverkey-qt.exe
 
 cd /d %~dp0
 
